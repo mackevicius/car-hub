23166e82a474b79b14694fd2a0075ef16f05888b
Povilas <povilas.mackevicius@stud.vilniustech.lt>
2024-02-11 13:18:39 +1300

    game card height fix

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,10 +1,4 @@
-import {
-  Card,
-  CardBody,
-  Heading,
-  HStack,
-  Image
-} from '@chakra-ui/react';
+import { Card, CardBody, Heading, HStack, Image } from '@chakra-ui/react';
 import { Link } from 'react-router-dom';
 import Game from '../entities/Game';
 import getCroppedImageUrl from '../services/image-url';
@@ -18,14 +12,12 @@ interface Props {
 
 const GameCard = ({ game }: Props) => {
   return (
-    <Card>
+    <Card height={'100%'}>
       <Image src={getCroppedImageUrl(game.background_image)} />
       <CardBody>
         <HStack justifyContent="space-between" marginBottom={3}>
           <PlatformIconList
-            platforms={game.parent_platforms?.map(
-              (p) => p.platform
-            )}
+            platforms={game.parent_platforms?.map((p) => p.platform)}
           />
           <CriticScore score={game.metacritic} />
         </HStack>
